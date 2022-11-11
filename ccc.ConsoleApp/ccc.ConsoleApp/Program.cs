dcd5cf3b561d6657f417bf36ca4ca17c6240c64f
Leon_Leeb <you.t5be@gmx.at>
2022-11-11 10:11:22 +0100

    mad a count all letters method

--- ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
+++ ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
@@ -5,4 +5,17 @@
         // Display the number of command line arguments.
         Console.WriteLine(args.Length);
     }
+
+    static int CountAllLetters(string inp, char sol)
+    {
+        int count = 0;
+        for(int i = 0; i < inp.Length; i++)
+        {
+            if(sol == inp[i])
+            {
+                count++;
+            }
+        }
+        return count;
+    }
 }
