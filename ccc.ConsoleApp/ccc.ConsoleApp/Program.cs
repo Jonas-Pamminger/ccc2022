8cb7de103cc31b0033712b0b170a4ee972d833d8
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2022-11-11 10:07:26 +0100

    fortnite

--- ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
+++ ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+    }
+}
