29a9a2dfda18fd6886916e7bbd6ab5a33a83c7df
Leon_Leeb <you.t5be@gmx.at>
2022-11-11 10:55:58 +0100

    did a ting

--- ccc.ConsoleApp/ccc.ConsoleApp/AlienMessage.cs
+++ ccc.ConsoleApp/ccc.ConsoleApp/AlienMessage.cs
@@ -6,22 +6,88 @@ using System.Threading.Tasks;
 
 namespace ccc.ConsoleApp
 {
-    
+
     internal class AlienMessage
     {
-        public string Message { get; set; }
+        public string[] Message { get; set; }
+        private const string FILE_NAME1 = "level2_1.in";
+        private const string FILE_NAME2 = "level2_2.in";
+        private const string FILE_NAME3 = "level2_3.in";
+        private const string FILE_NAME4 = "level2_4.in";
+        private const string FILE_NAME5 = "level2_5.in";
 
-        public int CountB()
+        public string[] Read()
+        {
+            string[] lines = File.ReadAllLines(FILE_NAME1);
+            Message = lines.Skip(1).ToArray();
+            return lines.Skip(1).ToArray();
+        }
+
+        public string CountLetter(char letter)
         {
             int c = 0;
-            foreach(char l in Message)
+            foreach (string line in Message)
             {
-                if(l == 'B')
+                foreach (char l in line)
                 {
-                    c++;
+                    if (l == letter)
+                    {
+                        c++;
+                    }
                 }
             }
-            return c;
+            return Convert.ToString(c);
+        }
+        public char[] returnDictionary(string[] inp)
+        {
+            char[] result = new char[CountNumberOfDiffernetLetter(inp)];
+            int count = 0;
+            string usedChars = "";
+            bool isIn;
+            for (int i = 0; i < inp.Length; i++)
+            {
+                isIn = false;
+                for (int n = 0; n < usedChars.Length; n++)
+                {
+                    if (usedChars[n] == inp[i][0])
+                    {
+                        isIn = true;
+                    }
+                }
+                if (!isIn && count < result.Length)
+                {
+                    usedChars += inp[i][0];
+                    result[count] = inp[i][0];
+                    count++;
+                }
+            }
+            return result;
+        }
+        public static int CountNumberOfDiffernetLetter(string[] strings)
+        {
+            string inp = "";
+            bool isIn;
+            for (int i = 0; i < strings.Length; i++)
+            {
+                for (int n = 0; n < strings[i].Length; n++)
+                {
+                    isIn = false;
+                    for (int j = 0; j < inp.Length; j++)
+                    {
+                        if (strings[i][n] == inp[j])
+                        {
+                            isIn = true;
+                        }
+                    }
+                    if (!isIn)
+                    {
+                        inp += strings[i][n];
+                    }
+
+                }
+            }
+            return inp.Length;
+
         }
     }
 }
--- ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
+++ ccc.ConsoleApp/ccc.ConsoleApp/Program.cs
@@ -1,21 +1,21 @@
-﻿class TestClass
+﻿using ccc.ConsoleApp;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+class TestClass
 {
     static void Main(string[] args)
     {
+
         // Display the number of command line arguments.
-        Console.WriteLine(args.Length);
+        AlienMessage alienMessage = new AlienMessage();
+        StringBuilder str = new StringBuilder();
+        foreach (var letter in alienMessage.returnDictionary(alienMessage.Read()))
+        {
+            str.AppendLine((alienMessage.CountLetter(letter)));
+        }
+        Console.WriteLine(str.ToString());
+
     }
 
-    static int CountAllLetters(string inp, char sol)
-    {
-        int count = 0;
-        for(int i = 0; i < inp.Length; i++)
-        {
-            if(sol == inp[i])
-            {
-                count++;
-            }
-        }
-        return count;
-    }
 }
