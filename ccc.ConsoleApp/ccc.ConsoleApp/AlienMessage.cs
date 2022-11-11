9b9dbebb7c529d987ad289f74e87ac9d40b2d8a9
Jonas Pamminger <j.pamminger@HTBLALeonding.onmicrosoft.com>
2022-11-11 10:14:38 +0100

    asfasd

--- ccc.ConsoleApp/ccc.ConsoleApp/AlienMessage.cs
+++ ccc.ConsoleApp/ccc.ConsoleApp/AlienMessage.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ccc.ConsoleApp
 {
+    
     internal class AlienMessage
     {
+        public string Message { get; set; }
+
+        public int CountB()
+        {
+            int c = 0;
+            foreach(char l in Message)
+            {
+                if(l == 'B')
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
     }
 }
